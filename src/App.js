81d90839e90ffe356d5103b9a2c6fb071ae37856
farhanarikta <farhnarikta@gmail.com>
2022-10-21 20:10:18 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Blog from "./components/Blog/Blog";
+import Study from "./components/Study/Study";
+
+function App() {
+  return (
+    <div className="body-main">
+      <Study></Study>
+      <Blog></Blog>
+    </div>
+  );
+}
+
+export default App;
